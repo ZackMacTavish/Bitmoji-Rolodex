1a4c3e55b757435c76aebe24c635aac963b04f93
ZackMacTavish <zmactavish@gmail.com>
2020-05-13 14:25:21 -0400

    Updates

---
+++ precache-manifest.47abd287995282bfe395e25cd7f1ff5f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38f091a924e7f2330e7b441110d7c8e7",
+    "url": "/Bitmoji-Rolodex/index.html"
+  },
+  {
+    "revision": "93ca11aa8ede5678cfb3",
+    "url": "/Bitmoji-Rolodex/static/css/main.89673a11.chunk.css"
+  },
+  {
+    "revision": "f7b84cd810271619eca9",
+    "url": "/Bitmoji-Rolodex/static/js/2.7d94dc6b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Bitmoji-Rolodex/static/js/2.7d94dc6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93ca11aa8ede5678cfb3",
+    "url": "/Bitmoji-Rolodex/static/js/main.1a2236a0.chunk.js"
+  },
+  {
+    "revision": "3b8fe4fff67166f7062a",
+    "url": "/Bitmoji-Rolodex/static/js/runtime-main.bc9809c8.js"
+  },
+  {
+    "revision": "7c828b7c217c8cf24d2b685fea8be04f",
+    "url": "/Bitmoji-Rolodex/static/media/noah-bold-webfont.7c828b7c.woff2"
+  },
+  {
+    "revision": "f1bb2b9ed6edd6d8108051fa1b2e65a6",
+    "url": "/Bitmoji-Rolodex/static/media/noah-bold-webfont.f1bb2b9e.woff"
+  }
+]);
